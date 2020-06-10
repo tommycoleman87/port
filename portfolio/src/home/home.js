2e766dab43e2640da34500ea6e340d0d2d309f25
Tommy Coleman <tommycoleman@Tommys-MacBook-Pro.local>
2020-06-10 17:55:23 -0400

    wip - homepage styling

--- portfolio/src/home/home.js
+++ portfolio/src/home/home.js
@@ -3,11 +3,9 @@ import { HomeContainer , SquareContainer, CircleContainer} from './homeStyles'
 const Home = () => {
     return (
         <HomeContainer>
-            <SquareContainer>
-            <CircleContainer>
-            Home
-            </CircleContainer>
-            </SquareContainer>
+           
+                    Hello, I am Tommy. I'm a fullstack developer in Florida. I have a passion for puzzles, problem solving, and creating. Willing to relocate.
+              
         </HomeContainer>
     )
 }
--- portfolio/src/home/homeStyles.js
+++ portfolio/src/home/homeStyles.js
@@ -4,29 +4,12 @@ export const HomeContainer = styled.div`
     display: flex;
     margin: auto;
     margin-top: 25px;
-    justify-content: center;
-`
-
-export const SquareContainer = styled.div`
-    position: absolute;
-    top: 9px;
-    left: 9px;
-    border: 1px solid #cc5500;
-    width: 500px;
     height: 500px;
-    color: white;
-`
-
-export const CircleContainer = styled.div`
-    display: flex;
+    width: 500px;
     justify-content: center;
     align-items: center;
-    position: relative;
-    top: -1px;
-    left: -1px;
-    width: 502px;
-    height: 502px;
-    border-radius: 25%;
-    background-color: black;
-
+    color: white;
+    border-bottom: 5px solid red;
+    border-top: 5px solid red;
 `
+
